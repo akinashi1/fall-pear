54a50bd781bddce6a493ccdd099d45fa0c8b76aa
ryouya <ryouya2000@gmail.com>
2022-10-20 08:52:00 +0900

    インデント修正

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,12 +3,12 @@
 <?php if(session('new_exe')){echo "<h4 class='text-danger mt-5 container-sm'>".session('new_exe')."</h4>";}; ?>
 
 <form class="card mt-5 container-sm" action="login_exe" method="post">
-    @csrf
-    <h5 class="card-header">ログイン</h5>
+	@csrf
+	<h5 class="card-header">ログイン</h5>
 
-    <?php echo "<div class='text-danger'>".session('login_error')."</div>"; ?>
+	<?php echo "<div class='text-danger'>".session('login_error')."</div>"; ?>
 
-    <div class="mb-3 mt-3 row">
+	<div class="mb-3 mt-3 row">
         <label for="inputmail" class="col-sm-2 col-form-label">メールアドレス</label>
         <div class="col-sm-10">
             <input type="text" class="form-control" id="inputmail" name="email">
@@ -28,30 +28,30 @@
 </form>
 
 <form class="card mt-5 container-sm" action="new_account" method="post">
-    @csrf
-    <h5 class="card-header">新規登録</h5>
+	@csrf
+	<h5 class="card-header">新規登録</h5>
 
-    <div class="mb-3 mt-3 row">
-        <label for="inputmail" class="col-sm-2 col-form-label" >ユーザーネーム(20文字)</label>
-        <div class="col-sm-10">
-            <div class="text-danger">{{ $errors->first('name') }}</div>
-            <input type="text" class="form-control" id="inputmail" name="name">
-        </div>
-    </div>
+	<div class="mb-3 mt-3 row">
+		<label for="inputmail" class="col-sm-2 col-form-label" >ユーザーネーム(20文字)</label>
+		<div class="col-sm-10">
+			<div class="text-danger">{{ $errors->first('name') }}</div>
+			<input type="text" class="form-control" id="inputmail" name="name">
+		</div>
+	</div>
 
-    <div class="mb-3 row">
-        <label for="inputmail" class="col-sm-2 col-form-label">メールアドレス</label>
-        <div class="col-sm-10">
+	<div class="mb-3 row">
+		<label for="inputmail" class="col-sm-2 col-form-label">メールアドレス</label>
+		<div class="col-sm-10">
 
-            <div class="text-danger">{{ $errors->first('email') }}</div>
+			<div class="text-danger">{{ $errors->first('email') }}</div>
 
-            <?php echo "<div class='text-danger'>".session('email_error')."</div>"; ?>
-            <div class="text-danger"></div>
-            <div class="text-danger"></div>
+			<?php echo "<div class='text-danger'>".session('email_error')."</div>"; ?>
+			<div class="text-danger"></div>
+			<div class="text-danger"></div>
 
-            <input type="text" class="form-control" id="inputmail" name="email">
-        </div>
-    </div>
+			<input type="text" class="form-control" id="inputmail" name="email">
+		</div>
+	</div>
 
 	<div class="mb-3 row">
 		<label for="inputPassword" class="col-sm-2 col-form-label">パスワード</label>
